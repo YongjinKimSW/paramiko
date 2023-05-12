7fbdaa9e5989a5bd7886ca255279eab8a51e0dcb
YongjinKimSW <yj74.kim@lge.com>
2023-05-12 15:26:17 +0900

    Update _version.py

--- paramiko/_version.py
+++ paramiko/_version.py
@@ -1,6 +1,5 @@
-__version_info__ = (2, 7, '3+tas')
-__version__ = ".".join(map(str, __version_info__))
-# __version__ = ".".join(map(str, __version_info__)) + "-tas"
+__version_info__ = (2, 7, 3)
+__version__ = ".".join(map(str, __version_info__)) + "+tas"
 
 
 
